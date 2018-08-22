722cc085c27545f7c1f7827db6296fcfd829127d
Fujiki Youta <c0nfl1c7.0613@gmail.com>
2018-08-22 16:26:03 +0900

    背景変更

--- src/scene/director.rb
+++ src/scene/director.rb
@@ -1,17 +1,23 @@
+
+
 module Ending
   class Director
-    BACKGROUND = Image.load('') #set background
+    BACKGROUND = Image.load('winner.jpg') #set background
     BGM = Sound.new('') #set BGM
 
     def initialize
       @player_images = {
-         #set character's image
+         'player1' => Image.load(''),
+         'player2' => Image.load('')
       }
     end
 
     def play
       Window.draw(0, 0, BACKGROUND)
 
+      #drawing winner's illust
+      #winner = ...
+      #img = @player_images[winner]
       if Input.key_push?(K_C) then
         Scene.current = :title #continue
       end
