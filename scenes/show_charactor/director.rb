9d9d9f39e3eae3b23dfea4f4835595c816f6c165
Yuhei FUJITA <fujita@devinker.com>
2018-08-23 17:19:58 +0900

    ゴングを追加

--- scenes/show_charactor/director.rb
+++ scenes/show_charactor/director.rb
@@ -2,7 +2,6 @@ module ShowCharactor
     class Director
         def initialize
             @font = Font.new(75, font_name="ＭＳ ゴシック")
-            @cnt = 0
             
             @player1 = Sprite.new(-100 - 500, -50, Image.load("images/select/card_#{Scene.players(:player1).downcase}.png"))
             @player1.scale_x = 0.5
@@ -11,6 +10,8 @@ module ShowCharactor
             @player2.scale_x = 0.5
             @player2.scale_y = 0.5
             
+            @sound = Sound.new('sound/show_charactor/gong.wav')
+            @cnt = 0
         end
 
         def play
@@ -20,6 +21,13 @@ module ShowCharactor
             @player2.x -= 10 if 300 <= @player2.x
             @player1.draw
             @player2.draw
+
+            @cnt += 1
+            @sound.play if @cnt == 40
+            if @cnt == 360
+                Scene.current = :game
+                Scene.add(:game, Game::Director.new)
+            end
         end
     end
 end
